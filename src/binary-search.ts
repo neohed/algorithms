7043837fa23bd0ddbaea41430ac48ab69014cb3b
neohed <35173474+neohed@users.noreply.github.com>
2022-05-06 23:11:16 +0100

    Update binary-search.ts

--- src/binary-search.ts
+++ src/binary-search.ts
@@ -1,5 +1,23 @@
-function search(items, item) {
+function search(items: (number | string)[], item: number | string): number | undefined {
+  let low = 0;
+  let high = items.length;
 
+  while (low <= high) {
+    const mid = (low + high) / 2;
+    const guess = items[mid];
+
+    if (guess === item) {
+      return mid
+    }
+
+    if (guess > item) {
+      high = mid - 1
+    } else {
+      low = mid + 1
+    }
+  }
+
+  return undefined
 }
 
 export default search
