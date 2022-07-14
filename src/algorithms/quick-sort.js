e01a0641b9143530c238271689a1ddd3212f0b6c
neohed <35173474+neohed@users.noreply.github.com>
2022-07-14 18:07:58 +0100

    Update quick-sort.js

--- src/algorithms/quick-sort.js
+++ src/algorithms/quick-sort.js
@@ -3,7 +3,9 @@ function quickSort(array) {
     return array
   }
 
-  const pivot = array.shift();
+  const pivotPoint = Math.floor(Math.random() * array.length);
+  const pivot = array[pivotPoint];
+  array.splice(pivotPoint, 1);
   const less = array.filter(v => v <= pivot);
   const greater = array.filter(v => v > pivot);
 
