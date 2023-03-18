05d17f4da2fe78f2051ff3a53ffb8dba4b873a17
neohed <35173474+neohed@users.noreply.github.com>
2023-03-18 11:05:13 +0000

    Create puzzle01.js

---
+++ src/puzzle01.js
@@ -0,0 +1,15 @@
+const s1 = 'SEND';
+const s2 = 'MORE';
+const s3 = 'MONEY';
+
+const letters = 'SENDMORY';
+
+const numbers = [0,2,3,4,5,6,7,8,9]
+
+for (let l in letters) {
+  console.log(letters[l])
+}
+
+for (let n in numbers) {
+  console.log(numbers[n])
+}
